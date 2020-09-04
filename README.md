a605207cb220ed3f88fbc93ac6472c188b865ad2
Bernd Kampe <pikatech@mail.de>
2020-09-04 11:52:41 +0200

    Project setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# Query proxy between the data portal of AquaDiva and Elasticsearch
+The semantic search in the AquaDiva data portal is powered by a domain-specific ontology. This proxy translates ontology IDs into Elasticsearch search terms and queries a specifically prepared index.
---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ad_query_proxy",
+    version="0.0.1",
+    author="Bernd Kampe",
+    author_email="bernd.kampe@uni-jena.de",
+    description="This proxy translates ontology IDs into Elasticsearch search terms and queries a specifically prepared index.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AquaDiva-INFRA1/ad-query-proxy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Science/Research"
+    ],
+    python_requires='>=3.6',
+)
