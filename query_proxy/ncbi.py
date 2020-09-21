527a4ef395381bcbd6ccf2805b6381a21e1a82b6
Bernd Kampe <pikatech@mail.de>
2020-09-21 20:24:21 +0200

    Implement file downloads

--- query_proxy/ncbi.py
+++ query_proxy/ncbi.py
@@ -7,12 +7,18 @@ Created on Mon Sep 21 13:37:22 2020
 """
 
 from ftplib import FTP, error_perm
+import hashlib
 import logging
+import os
+import re
 from typing import List
 
+import requests
+
+MD5_MATCHER = re.compile(b'MD5\(.+?\)= ([0-9a-fA-F]{32})')
 NCBI_SERVER = "ftp.ncbi.nlm.nih.gov"
 BASELINE_DIR = "pubmed/baseline"
-
+PATH = "." # TODO: turn this into a parameter
 
 def list_ncbi_files(path: str) -> List[str]:
     ftp = FTP(NCBI_SERVER)
@@ -43,6 +49,26 @@ archives = sorted(
     for name in names
     if "type" in name[1] and name[1]["type"] == "file" and name[0].endswith("xml.gz")
 )
+
 for archive in archives:
     if not archive + ".md5" in md5:
         logging.warn("No md5 checksum available for %s", archive)
+    archive_url = f"https://{NCBI_SERVER}/{BASELINE_DIR}/{archive}"
+    r = requests.get(archive_url, stream=True)
+    with open(os.path.join(PATH, archive), 'wb') as fd:
+        for chunk in r.iter_content(chunk_size=1_048_576):
+            fd.write(chunk)
+    r = requests.get(archive_url + ".md5")    
+    match = MD5_MATCHER.match(r.content)
+    md5 = hashlib.md5()
+    try:
+        with open(os.path.join(PATH, archive), 'rb') as compare_this:
+            block = compare_this.read(4096)
+            while len(block) != 0:
+                md5.update(block)
+                block = compare_this.read(4096)
+    except Exception as e:
+        logging.error(e)
+    digest = md5.hexdigest()
+    if digest != match.group(1):
+        pass
