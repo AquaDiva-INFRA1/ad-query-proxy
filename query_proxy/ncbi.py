d8829ae2f5fa3ffdc87ca3d016ecd403ebbb09d4
Bernd Kampe <pikatech@mail.de>
2020-09-21 20:27:37 +0200

    Code refactoring

--- query_proxy/ncbi.py
+++ query_proxy/ncbi.py
@@ -15,10 +15,11 @@ from typing import List
 
 import requests
 
-MD5_MATCHER = re.compile(b'MD5\(.+?\)= ([0-9a-fA-F]{32})')
+MD5_MATCHER = re.compile(b"MD5\(.+?\)= ([0-9a-fA-F]{32})")
 NCBI_SERVER = "ftp.ncbi.nlm.nih.gov"
 BASELINE_DIR = "pubmed/baseline"
-PATH = "." # TODO: turn this into a parameter
+PATH = "."  # TODO: turn this into a parameter
+
 
 def list_ncbi_files(path: str) -> List[str]:
     ftp = FTP(NCBI_SERVER)
@@ -36,39 +37,45 @@ def list_ncbi_files(path: str) -> List[str]:
     return names
 
 
-names = list_ncbi_files(BASELINE_DIR)
-md5 = set(
-    name[0]
-    for name in names
-    if "type" in name[1]
-    and name[1]["type"] == "file"
-    and name[0].endswith("xml.gz.md5")
-)
-archives = sorted(
-    name[0]
-    for name in names
-    if "type" in name[1] and name[1]["type"] == "file" and name[0].endswith("xml.gz")
-)
+def process_archives(filenames: str):
+    md5 = set(
+        name[0]
+        for name in filenames
+        if "type" in name[1]
+        and name[1]["type"] == "file"
+        and name[0].endswith("xml.gz.md5")
+    )
+    archives = sorted(
+        name[0]
+        for name in filenames
+        if "type" in name[1]
+        and name[1]["type"] == "file"
+        and name[0].endswith("xml.gz")
+    )
 
-for archive in archives:
-    if not archive + ".md5" in md5:
-        logging.warn("No md5 checksum available for %s", archive)
-    archive_url = f"https://{NCBI_SERVER}/{BASELINE_DIR}/{archive}"
-    r = requests.get(archive_url, stream=True)
-    with open(os.path.join(PATH, archive), 'wb') as fd:
-        for chunk in r.iter_content(chunk_size=1_048_576):
-            fd.write(chunk)
-    r = requests.get(archive_url + ".md5")    
-    match = MD5_MATCHER.match(r.content)
-    md5 = hashlib.md5()
-    try:
-        with open(os.path.join(PATH, archive), 'rb') as compare_this:
-            block = compare_this.read(4096)
-            while len(block) != 0:
-                md5.update(block)
+    for archive in archives:
+        if not archive + ".md5" in md5:
+            logging.warn("No md5 checksum available for %s", archive)
+        archive_url = f"https://{NCBI_SERVER}/{BASELINE_DIR}/{archive}"
+        r = requests.get(archive_url, stream=True)
+        with open(os.path.join(PATH, archive), "wb") as fd:
+            for chunk in r.iter_content(chunk_size=1_048_576):
+                fd.write(chunk)
+        r = requests.get(archive_url + ".md5")
+        match = MD5_MATCHER.match(r.content)
+        md5 = hashlib.md5()
+        try:
+            with open(os.path.join(PATH, archive), "rb") as compare_this:
                 block = compare_this.read(4096)
-    except Exception as e:
-        logging.error(e)
-    digest = md5.hexdigest()
-    if digest != match.group(1):
-        pass
+                while len(block) != 0:
+                    md5.update(block)
+                    block = compare_this.read(4096)
+        except Exception as e:
+            logging.error(e)
+        digest = md5.hexdigest()
+        if digest != match.group(1):
+            pass
+
+
+names = list_ncbi_files(BASELINE_DIR)
+process_archives(names)
