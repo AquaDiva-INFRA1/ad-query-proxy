e1b35c21589c4d19cca7038e2c65251906ea8e4a
Bernd Kampe <pikatech@mail.de>
2020-10-05 12:43:07 +0200

    Build trie automaton for document tagging
    Entries from the NCBI Taxonomy are taken from the original ontology,
    instead of the AquaDiva ontology. This way, absent taxonomic information
    can be restored. The tagger will make use of this information and the
    additional synonyms.

---
+++ preprocessing/ncbi_filter.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Functions to process the NCBI Taxonomy.
+
+Created on Wed May 15 13:10:06 2019
+
+@author: Bernd Kampe
+"""
+
+from typing import cast, Dict, Iterator, List, Set, Union
+from pathlib import Path
+import logging
+import re
+
+logger = logging.getLogger("ncbi")
+
+
+FIELDS = [
+    "ID",
+    "PARENT ID",
+    "RANK",
+    "GC ID",
+    "MGC ID",
+    "SCIENTIFIC NAME",
+    "ANAMORPH",
+    "BLAST NAME",
+    "COMMON NAME",
+    "EQUIVALENT NAME",
+    "GENBANK ACRONYM",
+    "GENBANK ANAMORPH",
+    "GENBANK COMMON NAME",
+    "GENBANK SYNONYM",
+    "IN-PART",
+    "MISNOMER",
+    "MISSPELLING",
+    "SYNONYM",
+    "TELEOMORPH",
+    "INCLUDES",
+    "ACRONYM",
+]
+UNIQUE = frozenset(["ID", "GC ID", "MGC ID", "PARENT ID", "RANK"])
+
+TAXONOMIC = [
+    "SCIENTIFIC NAME",
+    "SYNONYM",
+    "GENBANK SYNONYM",
+    "EQUIVALENT NAME",
+    "MISSPELLING",
+    "TELEOMORPH",
+    "ANAMORPH",
+    "GENBANK ANAMORPH",
+    "MISNOMER",
+    "IN-PART",
+]
+COMMON_NAMES = ["GENBANK COMMON NAME", "COMMON NAME", "BLAST NAME"]
+
+FIELD_REGEX = re.compile("(" + "|".join(FIELDS) + r")\s+:\s")
+DELIMITER = re.compile("//")
+
+
+def taxonomy2dict(
+    taxonomy: Union[Path, str]
+) -> Iterator[Dict[str, Union[str, List[str]]]]:
+    """
+    Read in the NCBI Taxonomy as processed by the European Bioinformatics Institute.
+
+    taxonomy: path to the taxonomy file
+
+    Yields entries as dictionaries.
+    """
+    with open(taxonomy, "rt") if isinstance(taxonomy, str) else taxonomy.open(
+        "rt"
+    ) as tax:
+        first = True
+        entry: Dict[str, Union[str, List[str]]] = dict()
+        error = False
+        for i, line in enumerate(tax):
+            if error:
+                if DELIMITER.match(line):
+                    entry = dict()
+                    first = True
+                    error = False
+                    if "ID" in entry:
+                        logging.warning("Skipped entry %d", entry["ID"])
+            elif first:
+                tax_id = FIELD_REGEX.match(line)
+                if tax_id is None:
+                    logging.warning("Missing ID on line %d", i)
+                    error = True
+                else:
+                    entry["ID"] = line[tax_id.end() :].rstrip()
+                    first = False
+            elif DELIMITER.match(line):
+                yield entry
+                entry = dict()
+                first = True
+                error = False
+            else:
+                field = FIELD_REGEX.match(line)
+                if field is None:
+                    logging.warning("Unknown format on line %d", i)
+                    logging.warning("line %d: %s", i, line)
+                    error = True
+                else:
+                    match = FIELD_REGEX.match(line)
+                    if match:
+                        if field.group(1) in UNIQUE:
+                            entry[field.group(1)] = line[field.end() :].rstrip()
+                        else:
+                            values = cast(List[str], entry.get(field.group(1), []))
+                            values.append(line[field.end() :].rstrip())
+                            entry[field.group(1)] = values
+
+
+def make_variants(tax_entry: Dict[str, Union[str, List[str]]]) -> Set[str]:
+    """Generate spelling variants as described in the LINNAEUS paper."""
+    variants = set()
+    for name in TAXONOMIC:
+        values = tax_entry.get(name, [])
+        for value in values:
+            tokens = value.split()
+            if len(tokens) == 1:
+                variants.add(value)
+            elif tokens[0].isalpha() and tokens[1].isalpha():
+                # Original spelling
+                variants.add(value)
+                # Original spelling, all lowercase
+                variants.add(value.lower())
+                # Abbreviated genus
+                tokens[0] = tokens[0][0] + "."
+                variants.add(" ".join(tokens))
+                # Abbreviated genus, all lowercase
+                variants.add(" ".join(tokens).lower())
+                # Abbreviated genus, no space after genus
+                variant = tokens[0] + " ".join(tokens[1:])
+                variants.add(variant)
+                # Abbreviated genus, no space after genus, all lowercase
+                variants.add(variant.lower())
+            else:
+                variants.add(value)
+    for name in COMMON_NAMES:
+        values = tax_entry.get(name, [])
+        for value in values:
+            variants.add(value)
+            variants.add(value[0].upper() + value[1:])
+    return variants
+
+
+def filter_NCBI_taxonomy(
+    file: Union[Path, str], ids: List[str]
+) -> Dict[str, List[str]]:
+    """
+    Create a dictionary containing all name variants of specific entries.
+    
+    Parameters
+    ----------
+    file : Union[Path, str]
+        Path to taxonomy.dat
+    ids : List[str]
+        A list of all IDs of interest
+    
+    Returns
+    -------
+    Dict[str, List[str]]
+        All generated name variants stored by ID.
+    
+    """
+    entries = dict()
+    if not ids is None:
+        for entry in taxonomy2dict(file):
+            if not entry["ID"] in ids:
+                continue
+            variants = sorted(make_variants(entry))
+            entries[entry["ID"]] = variants
+    return entries
---
+++ preprocessing/onto2trie.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  1 12:48:19 2020
+
+@author: Bernd Kampe
+"""
+
+import argparse
+from pathlib import Path
+import pickle
+import sys
+from typing import Dict, List, Tuple
+
+from ahocorasick import Automaton
+import pronto
+
+from ncbi_filter import filter_NCBI_taxonomy
+
+
+def onto2dict(ontology: pronto.Ontology) -> Tuple[Dict[str, List[str]], List[str]]:
+    """
+    
+
+    Parameters
+    ----------
+    ontology : pronto.Ontology
+        An ontology to be later turned into a specialized dictionary to be
+        used for Named Entity Recognition
+
+    Returns
+    -------
+    Tuple[Dict[str, List[str]], List[str]]
+        The first entry of the tuple is a dictionary with lists of all 
+        names and synonyms for all keys that are not in the NCBI Taxonomy.
+        The second entry is a list of all IDs that originate from the
+        NCBI Taxonomy (except the root, if present).
+
+    """
+    concepts = dict()
+    taxon_keys = []
+    for key in ontology:
+        if key.startswith("NCBITaxon:") and not key == "NCBITaxon:1":
+            taxon_keys.append(key[10:])
+        else:
+            names = set()
+            entry = ontology[key]
+            if entry.name:
+                names.add(entry.name)
+                if not entry.name.isupper():
+                    names.add(entry.name[0].upper() + entry.name[1:])
+            if entry.synonyms:
+                names.update(synonym.description for synonym in entry.synonyms)
+                names.update(
+                    synonym.description[0].upper() + synonym.description[1:]
+                    for synonym in entry.synonyms
+                    if not synonym.description.isupper()
+                )
+            concepts[key] = sorted(names)
+    return concepts, taxon_keys
+
+
+def make_automaton(concepts):
+    """Create an Aho-Corasick automaton out of dictionary entries."""
+    automaton = Automaton()
+    entries = dict()
+    for label, values in concepts.items():
+        for v in values:
+            if v in entries:
+                dictvalue = list(entries[v])
+                dictvalue.append(label)
+                dictvalue = tuple(dictvalue)
+                entries[v] = dictvalue
+            else:
+                entries[v] = (label,)
+
+    for key in entries.keys():
+        if key != "":
+            _ = automaton.add_word(key, (key, entries[key]))
+
+    automaton.make_automaton()
+    return automaton
+
+
+if __name__ == "__main__":
+    PARSER = argparse.ArgumentParser(
+        description="Converts an ontology into an Aho-Corasick automaton and writes it to disk.\n"
+        + "The NCBI Taxonomy is used to augment the information about taxons and generate"
+        + "name variants of entries as LINNAEUS would do."
+    )
+    PARSER.add_argument(
+        "-i", "--input", help="Path to an ontology that should be converted", type=str
+    )
+    PARSER.add_argument("-n", "--ncbi", help="Path to the NCBI Taxonomy", type=str)
+    PARSER.add_argument(
+        "-o", "--output", help="Where the automaton should be written to", type=str
+    )
+    ARGS = PARSER.parse_args()
+    INPUT = Path(ARGS.input)
+    if not INPUT.exists():
+        print(f"ERROR: Input file {INPUT} does not exist.", file=sys.stderr)
+        sys.exit(1)
+    if not INPUT.is_file():
+        print(f"ERROR: Input argument {INPUT} is not a file.", file=sys.stderr)
+        sys.exit(1)
+    NCBI = Path(ARGS.ncbi)
+    if not NCBI.exists():
+        print(f"ERROR: Input file {NCBI} does not exist.", file=sys.stderr)
+        sys.exit(1)
+    if not NCBI.is_file():
+        print(f"ERROR: Input argument {NCBI} is not a file.", file=sys.stderr)
+        sys.exit(1)
+    OUTPUT = Path(ARGS.output)
+    if OUTPUT.exists():
+        print(f"ERROR: Output file {OUTPUT} already exists.", file=sys.stdout)
+        sys.exit(1)
+    try:
+        with INPUT.open("rb") as onto_input:
+            ontology = pronto.Ontology(onto_input)
+    except KeyError as e:
+        print(f"ERROR: {e}", file=sys.stderr)
+        sys.exit(2)
+    concepts, taxon_keys = onto2dict(ontology)
+    variants = filter_NCBI_taxonomy(NCBI, taxon_keys)
+    for key, values in variants.items():
+        concepts["NCBITaxon:" + key] = values
+    trie = make_automaton(concepts)
+    with OUTPUT.open("wb") as output:
+        pickle.dump(trie, output)
