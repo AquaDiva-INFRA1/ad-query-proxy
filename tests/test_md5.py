653013b80233de2d9f3cd66bce8bdc9cbd9c3e22
Bernd Kampe <pikatech@mail.de>
2020-10-06 11:46:46 +0200

    Test with changed regex

--- tests/test_md5.py
+++ tests/test_md5.py
@@ -9,23 +9,25 @@ Created on Mon Sep 21 16:26:31 2020
 import hashlib
 import re
 
+from query_proxy.ncbi import MD5_MATCHER
+
+
 def test_ncbi_m5():
     """Test if the checksum comparison works"""
-    MD5_MATCHER = re.compile(r'MD5\(.+?\)= ([0-9a-fA-F]{32})')
-    md5file = 'tests/resources/dummy.xml.gz.md5'
-    target_file = 'tests/resources/dummy.xml.gz'
-    md5sum = open(md5file, 'rt', encoding='utf-8').read()
-    
+    md5file = "tests/resources/dummy.xml.gz.md5"
+    target_file = "tests/resources/dummy.xml.gz"
+    md5sum = open(md5file, "rb").read()
+
     match = MD5_MATCHER.match(md5sum)
     md5 = hashlib.md5()
     try:
-        with open(target_file, 'rb') as compare_this:
+        with open(target_file, "rb") as compare_this:
             block = compare_this.read(4096)
             while len(block) != 0:
                 md5.update(block)
                 block = compare_this.read(4096)
     except Exception as e:
         print(e)
-    
+
     digest = md5.hexdigest()
-    assert digest == match.group(1)
+    assert digest == match.group(1).decode("utf-8")
